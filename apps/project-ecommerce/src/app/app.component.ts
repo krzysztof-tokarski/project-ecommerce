20723399c00385eb713894abb92b39ec909abbff
AEE <myfutureiscode@gmail.com>
2022-08-17 19:51:03 +0200

    feat: setup shell and some basic routing

--- apps/project-ecommerce/src/app/app.component.ts
+++ apps/project-ecommerce/src/app/app.component.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Message } from '@fullstack-node-portfolio-app/api-interfaces';
 
 @Component({
-  selector: 'fullstack-node-portfolio-app-root',
+  selector: 'ecommerce-app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  hello$ = this.http.get<Message>('/api/hello');
-  constructor(private http: HttpClient) {}
 }
--- apps/project-ecommerce/src/app/core/core.module.ts
+++ apps/project-ecommerce/src/app/core/core.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    HttpClientModule,
   ]
 })
 export class CoreModule { }
