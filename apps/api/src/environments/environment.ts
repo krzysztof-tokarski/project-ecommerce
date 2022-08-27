697ce0f6127f6415072685b3215f125009658843
krzysztof.tokarski <krzysztof.tokarski96@gmail.com>
2022-08-27 18:22:20 +0200

    fix: remove environment files

--- apps/api/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  DB_CONNECTION_STRING: process.env.CONNECTION_STRING,
-};
--- apps/project-ecommerce/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-
-export const environment = {
-  API_URL: "http://localhost:3333/api",
-  production: "false"
-};
