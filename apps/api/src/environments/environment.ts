dad3756af26ffef914ae9166507908712149a88d
KT <94806776+krzysztof-tokarski@users.noreply.github.com>
2022-08-27 17:37:11 +0200

    Delete environment.ts

--- apps/api/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  DB_CONNECTION_STRING: 'mongodb+srv://admin:pass123@cluster0.v0jr0th.mongodb.net/?retryWrites=true&w=majority',
-  production: 'false',
-  JWT_SECRET: 'SECRETKEY',
-};
