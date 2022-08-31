a48ab681cc8ced1bfb60187a798488b81570cb23
krzysztof.tokarski <krzysztof.tokarski96@gmail.com>
2022-08-31 23:27:41 +0200

    feat: add navbar to frame

--- apps/project-ecommerce/src/app/core/shell/shell.module.ts
+++ apps/project-ecommerce/src/app/core/shell/shell.module.ts
@@ -8,6 +8,10 @@ import { SharedModule } from '@shared/shared-module/shared.module';
 import { TopSearchbarComponent } from './header/top-searchbar/top-searchbar.component';
 import { TopDropdownComponent } from './header/top-dropdown/top-dropdown.component';
 import { TopCartComponent } from './header/top-cart/top-cart.component';
+import { MenubarModule } from 'primeng/menubar';
+import { TopNavbarComponent } from './header/top-navbar/top-navbar.component';
+
+const PRIMENG_MODULES = [MenubarModule];
 
 @NgModule({
   declarations: [
@@ -17,10 +21,12 @@ import { TopCartComponent } from './header/top-cart/top-cart.component';
     TopSearchbarComponent,
     TopDropdownComponent,
     TopCartComponent,
+    TopNavbarComponent,
   ],
   imports: [
     CommonModule,
     SharedModule,
+    PRIMENG_MODULES,
     RouterModule.forChild([
       {
         path: '',
