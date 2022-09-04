a34d472dc5a42d4b75607bcc54e57d593fb0c47b
krzysztof.tokarski <krzysztof.tokarski96@gmail.com>
2022-09-04 18:56:04 +0200

    feat: add swagger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,7 +5,7 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
@@ -14,6 +14,12 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
   app.enableCors();
+
+  const config = new DocumentBuilder().setTitle('Project Ecommerce').setDescription('Project Ecommerce API').setVersion('0.0').build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
 
   Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
